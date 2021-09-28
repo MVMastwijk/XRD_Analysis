de1de9168f0023e091119c45f46aaf8931308959
MVMastwijk <91330989+MVMastwijk@users.noreply.github.com>
2021-09-28 16:13:34 +0200

    Update and rename XRD_Analysis_2.py to XRD_Analysis_3.py

--- XRD_Analysis_3.py
+++ XRD_Analysis_3.py
@@ -1,3 +1,7 @@
+#XRD Powder Diffraction Analysis Program (for cubic crystal systems)
+#Author = MV Mastwijk, 28-SEP-2021
+#See below for the settings
+
 import numpy as np
 import matplotlib.pyplot as plt
 import math
@@ -8,12 +12,12 @@ import pathlib
 
 #settings
 
-filename = "cu2o_A-X_Y.txt"
+filename = "cu2o_A-X_Y.txt" #note: file must be in the same folder as the program
 cutoff_2xtheta = 12 #degrees
-peak_prominence = 20
+peak_prominence = 20 #higher peak prominence > small peaks are ignored
 
 wavelength = 0.71073 #A^0
-max_laue_sum_of_sq = 100
+laue_indices_sum_of_sq=[2,3,4,6,8,11,12,16,19,20] #when first running program, leave empty. determine the values by hand via 4sin(θ)^2/λ^2=l/a^2 and rerun
 
 #
 
@@ -28,8 +32,8 @@ fig, ax = plt.subplots(1, figsize=(8, 6), facecolor='whitesmoke')
 ax.set_facecolor("gainsboro")
 ax.grid(color='white', linestyle='-', linewidth=1)
 fig.suptitle(filename, fontsize=15)
-plt.xlabel("2x Theta (degrees)")
-plt.ylabel("Intensity (arbitrary units)")
+plt.xlabel("2θ (°)")
+plt.ylabel("I (arbitrary units)")
 ax.plot(x2theta, intensity, color="orange")
 plt.axvline(x=cutoff_2xtheta, linestyle='--', color="grey")
 
@@ -72,34 +76,48 @@ for i in range(len(peak_list)):
     plt.plot(parameters[1], parameters[0]+baseheight, ".", color="black")
     peak_position.append([parameters[1],np.sqrt(np.diag(info))[1]])
 
+print("-----")
 print("Peak positions and std. dev. (degrees):",peak_position) #entry n corresponds to peak n (n in N0), contains x2theta and std.dev. of peak, respectively. 
+print("-----")
 
-sum_of_sq=[]
-for i in range(0,max_laue_sum_of_sq+1):
-    for j in range(0,max_laue_sum_of_sq+1):
-        for k in range(0,max_laue_sum_of_sq+1):
-            if i**2+j**2+k**2 not in sum_of_sq and i**2+j**2+k**2<max_laue_sum_of_sq and i**2+j**2+k**2!=0:
-                sum_of_sq.append(i**2+j**2+k**2)
-sum_of_sq.sort()
-
-cell_parameter_possible=[]
+factor_list=[]
 for i in range(len(peak_position)):
-    cell_parameter_possible.append([])
-    for j in range(len(sum_of_sq)):
-        cell_parameter_possible[i].append(math.sqrt(sum_of_sq[j]*wavelength**2 /(4*math.sin(peak_position[i][0]*math.pi/180)**2)))
+    factor_list.append((4*math.sin(0.5*peak_position[i][0]*math.pi/180)**2)/wavelength**2)
 
-cell_parameter_possible.sort(reverse=True)
+cell_parameter=[]
+cell_parameter_variance=[]
+cell_volume=[]
+cell_volume_variance=[]
+firstrun=False
+if laue_indices_sum_of_sq==[]:
+    for i in range(len(peak_position)):
+        laue_indices_sum_of_sq.append(1)
+        firstrun=True
+
 for i in range(len(peak_position)):
-    for j in range(len(sum_of_sq)):
-        if cell_parameter_possible[i][j]/cell_parameter_possible[0][0]<0.9:
-            cell_parameter_possible[i][j]=0
-        if cell_parameter_possible[i][j]/cell_parameter_possible[len(peak_position)-1][len(sum_of_sq)-1]>1.1:
-            cell_parameter_possible[i][j]=0
+    cell_ai=1/math.sqrt(factor_list[i]/laue_indices_sum_of_sq[i])
+    cell_parameter.append(cell_ai)
+    cell_parameter_variance.append((cell_ai**2)/(math.tan(0.5*peak_position[i][0]*math.pi/180)**2)*(0.5*peak_position[i][1]*math.pi/180)**2)
+    cell_volume.append(cell_ai**3)
+    cell_volume_variance.append(9*cell_ai**4*cell_parameter_variance[i])
 
-cell_parameter_neat=cell_parameter_possible
-for i in range(len(peak_position)):
-    cell_parameter_neat[i] = [j for j in cell_parameter_possible[i] if j != 0]
+cell_parameter_avg=sum(cell_parameter)/len(peak_position)
+cell_parameter_variance_avg=sum(cell_parameter_variance)/(len(peak_position)**2)
 
-print("Possible cell lengths (A^0):",cell_parameter_neat)
+cell_volume_avg=sum(cell_volume)/len(peak_position)
+cell_volume_variance_avg=sum(cell_volume_variance)/(len(peak_position)**2)
+
+
+print("4sin(θ)^2/λ^2:",factor_list)
+print("-----")
+if firstrun==False:
+    print("Cell parameter (A^0):",cell_parameter_avg)
+    print("Cell parameter std. dev. (A^0):",math.sqrt(cell_parameter_variance_avg))
+    print("-----")
+    print("Cell volume (A^0^3):",cell_volume_avg)
+    print("Cell volume std. dev. (A^0^3):",math.sqrt(cell_volume_variance_avg))
+else:
+    print("Determine the sum of squares l of Laue indices belonging to each peak by 4sin(θ)^2/λ^2=l/a^2, put these in the settings in ascending order and rerun.")
+print("-----")
 
 plt.show()
